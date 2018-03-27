fc40a9c3caa98d7f60ef7cacd05a7a5f22cf29ed
peytonlwhite <36774084+peytonlwhite@users.noreply.github.com>
2018-03-26 22:21:39 -0500

    hello world

---
+++ PeytonWhiteHelloClass.java
@@ -0,0 +1,27 @@
+/*
+ * Name: Peyton White
+ * Date:Jan/19/2018
+ * Description: This program outputs information about me
+ * Class: 2070 - 08
+ */
+package peytonwhitehelloclass;
+
+/**
+ *
+ * @author Peyton
+ */
+public class PeytonWhiteHelloClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Mr.Church");
+        System.out.println("My name is Peyton White");
+        System.out.println("My Major is Computer science"
+                + "and my minor is information and security");
+        System.out.println("My A number: ApA00647859");
+        System.out.println("My favorite beverage is by far Sweat tea!!");
+    }
+
+}
